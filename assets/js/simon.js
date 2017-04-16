9f30f3fe8be24962cccf4aa8a5ec2452b1710090
Ashish Verma <ashishrodio@gmail.com>
2017-04-16 11:41:24 +0530

    Added on click Animations

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,8 +1,26 @@
-let blocks = Array.from(document.querySelectorAll('.block'));
+function playSound(currentBlock) {
+    let audio = document.querySelector(`audio[data-block="${currentBlock}"]`);
+    let block = document.querySelector('.' + currentBlock);
 
-blocks.forEach((block) => {
-    block.addEventListener('click', (event) => {
-        console.log(event.target.className);
-    });
+    block.classList.add('playing');
+    audio.play();
+}
+
+function removeTransition(event) {
+    console.log(event);
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('playing');
+}
+
+let blocks = document.querySelectorAll('.block');
+
+blocks.forEach(block => {
+    block.addEventListener('transitionend', e => removeTransition(e));
+    block.addEventListener('click',  (e) => playSound(e.target.classList[1]));
 });
 
+let randomBlock = Math.floor(Math.random() * 4) + 1;
+randomBlock = 'block' + randomBlock;
+
+//console.log(randomBlock);
+// setTimeout(playSound, 1000, randomBlock);
