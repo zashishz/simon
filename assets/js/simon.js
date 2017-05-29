859caf102b079b1faa4008b2ff5e985532c95a32
ashish.g.verma <ashish.g.verma@accenture.com>
2017-05-29 14:45:29 +0530

    Woring with user response - Validation on game over pending

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,3 +1,6 @@
+let computerMoves = [];
+let userMoves = [];
+
 function playSound(currentBlock) {
     let audio = document.querySelector(`audio[data-block="${currentBlock}"]`);
     let block = document.querySelector('.' + currentBlock);
@@ -7,7 +10,6 @@ function playSound(currentBlock) {
 }
 
 function removeTransition(event) {
-    console.log(event);
     if (event.propertyName !== 'transform') return;
     event.target.classList.remove('playing');
 }
@@ -16,11 +18,55 @@ let blocks = document.querySelectorAll('.block');
 
 blocks.forEach(block => {
     block.addEventListener('transitionend', e => removeTransition(e));
-    block.addEventListener('click',  (e) => playSound(e.target.classList[1]));
+    block.addEventListener('click', function () {
+        playSound(this.classList[1]);
+        userMoves.push(this.classList[1]);
+        //validate
+        let validationStatus = validateMoves();
+        if (validationStatus) {
+            setTimeout(computerTurn, 1000);
+        } else if(validationStatus == 'over'){
+            alert('game over');
+        }
+        console.log("userMoves", userMoves);
+    });
 });
 
-let randomBlock = Math.floor(Math.random() * 4) + 1;
-randomBlock = 'block' + randomBlock;
+function getRandomBlock() {
+    let randomBlock = Math.floor(Math.random() * 4) + 1;
+    return 'block' + randomBlock;
+}
 
 //console.log(randomBlock);
 // setTimeout(playSound, 1000, randomBlock);
+
+function computerTurn() {
+    // push a random block in Array
+    computerMoves.push(getRandomBlock());
+
+    // play playSound
+    for (let i = 0; i < computerMoves.length; i++) {
+        setTimeout(() => {
+            playSound(computerMoves[i]);
+        }, i * 1000);
+    }
+    // get user input
+    console.log("computerMoves", computerMoves);
+    //reset user moves
+    userMoves = [];
+    // check if pattern is correct
+    // repeat;
+}
+computerTurn();
+
+//check if user and computers input ae in sync
+function validateMoves() {
+    if (computerMoves.length == userMoves.length) {
+        for (let i = 0; i < computerMoves.length; i++) {
+            if (computerMoves[i] != userMoves[i]) return 'over';
+        }
+        return true;
+    }
+    return false;
+}
+
