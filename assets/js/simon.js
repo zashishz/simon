46100c1fd57c6f0ca4ae73b05d8ef090d0ccd9ef
Ashish <ashishrodio@gmail.com>
2017-06-07 00:22:40 +0530

    Barebones Working

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,5 +1,9 @@
 let computerMoves = [];
 let userMoves = [];
+let gameMode = false;
+let startBtnClicked = false;
+let strictMode = false;
+let computerTimer;
 
 function playSound(currentBlock) {
     let audio = document.querySelector(`audio[data-block="${currentBlock}"]`);
@@ -16,21 +20,48 @@ function removeTransition(event) {
 
 let blocks = document.querySelectorAll('.block');
 
+//Activate only if Game Mode ON
+
+
 blocks.forEach(block => {
     block.addEventListener('transitionend', e => removeTransition(e));
     block.addEventListener('click', function () {
-        playSound(this.classList[1]);
-        userMoves.push(this.classList[1]);
-        //validate
-        let validationStatus = validateMoves();
-        if (validationStatus) {
-            setTimeout(computerTurn, 1000);
-        } else if(validationStatus == 'over'){
-            alert('game over');
+        clearInterval(computerTimer);
+        if (gameMode && startBtnClicked) {
+            userMoves.push(this.classList[1]);
+            console.log("userMoves", userMoves);
+            //validate
+            let validationStatus = validateMoves();
+            if (validationStatus) {
+                playSound(this.classList[1]);
+                if (computerMoves.length == userMoves.length) {
+                    setTimeout(computerTurn, 1000);
+                }
+            } else if(!strictMode) {
+                //repeat same move  try again
+                userMoves = [];
+                console.log('try again');
+                triggerError();
+            } else {
+                //alert('game over');
+                //reste Game
+                // setTimeout(reset, 1000);
+                triggerError();
+                // gameMode = false;
+                console.error("Game Over - Press start to play again.");
+                //setTimeout(computerTurn, 1000);
+            }
         }
-        console.log("userMoves", userMoves);
     });
 });
+
+function reset() {
+    userMoves = [];
+    computerMoves = [];
+    document.getElementById('counter').textContent = 0;
+    clearInterval(computerTimer);
+    startBtnClicked = false;
+}
 
 function getRandomBlock() {
     let randomBlock = Math.floor(Math.random() * 4) + 1;
@@ -41,32 +72,82 @@ function getRandomBlock() {
 // setTimeout(playSound, 1000, randomBlock);
 
 function computerTurn() {
+    gameMode = false;
     // push a random block in Array
     computerMoves.push(getRandomBlock());
 
-    // play playSound
-    for (let i = 0; i < computerMoves.length; i++) {
-        setTimeout(() => {
-            playSound(computerMoves[i]);
-        }, i * 1000);
-    }
+    play(computerMoves);
     // get user input
     console.log("computerMoves", computerMoves);
     //reset user moves
     userMoves = [];
+    document.getElementById('counter').textContent = computerMoves.length;
+    computerTimer = setInterval(triggerError,5000);
     // check if pattern is correct
     // repeat;
+    gameMode = true;
 }
-computerTurn();
+
+function triggerError() {
+    let errorAudio = document.querySelector(`audio[data-block="error"]`);
+    errorAudio.play();
+    if(!strictMode) setTimeout(play,1000,computerMoves);
+    else { reset(); setTimeout(computerTurn,1000); }
+}
+
+function play(moves) {
+    // play playSound
+    for (let i = 0; i < moves.length; i++) {
+        setTimeout(() => {
+            playSound(moves[i]);
+        }, i * 1000);
+    }
+}
+
+let slider = document.querySelector('.slider-control');
+let switchBtn = document.querySelector('.switch');
+
+slider.addEventListener('click', () => {
+    gameMode = (gameMode == true) ? false : true;
+    if (gameMode) {
+        //move slider to right
+        switchBtn.classList.add('slider-play-on');
+    } else {
+        //move slider back to left
+        switchBtn.classList.remove('slider-play-on');
+        strictBulb.classList.remove('strict-bulb-on');
+        startBtnClicked = false;
+        reset();
+    }
+})
+
+let startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', () => {
+    if (gameMode && !startBtnClicked) {
+        startBtnClicked = true;
+        setTimeout(computerTurn, 1000);
+    }
+});
+
+let strictBtn = document.getElementById('strict-btn');
+let strictBulb = document.querySelector('.strict-bulb');
+strictBtn.addEventListener('click', () => {
+    if (gameMode) {
+        if (strictBulb.classList.contains('strict-bulb-on')) {
+            strictBulb.classList.remove('strict-bulb-on');
+            strictMode = false;
+        } else {
+            strictBulb.classList.add('strict-bulb-on');
+            strictMode = true;
+        }
+    }
+});
 
 //check if user and computers input ae in sync
 function validateMoves() {
-    if (computerMoves.length == userMoves.length) {
-        for (let i = 0; i < computerMoves.length; i++) {
-            if (computerMoves[i] != userMoves[i]) return 'over';
-        }
-        return true;
+    for (let i = 0; i < userMoves.length; i++) {
+        if (computerMoves[i] != userMoves[i]) return false;
     }
-    return false;
+    return true;
 }
 
